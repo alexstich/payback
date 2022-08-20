b9a00aafbd7185cdb0f5063c6c1bfb38ce5a0cdb
AlexStich <alex@rucode.org>
2022-08-20 14:03:46 +0400

    Initial Commit

---
+++ payback/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  payback
+//
+//  Created by Алексей Гребенкин on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
